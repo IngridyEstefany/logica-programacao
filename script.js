d94df9be52a88e58e1d95279de4d95bc09f9db5a
Ingridy Estefany <142455701+IngridyEstefany@users.noreply.github.com>
2023-10-05 19:45:59 +0000

    feat: interface do programa

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+function mediaAluno() {
+    
+
 //Lógica de Programação
 // Problema: obter 3 notas de um aluno, calcular a média,
 // exibir a média e exibir a situação do aluno: aprovado,
@@ -7,13 +10,15 @@ console.log('Início do programa');
 
 //declaração das variáveis
 
-const nota1 = parseInt(prompt('Digite a nota 1:'));
-const nota2 = parseInt(prompt('Digite a nota 2:'));
-const nota3 = parseInt(prompt('Digite a nota 3:'));
+const nota1 = parseInt(document.getElementById('nota1').value);
+const nota2 = parseInt(document.getElementById('nota2').value);
+const nota3 = parseInt(document.getElementById('nota3').value);
 
 
 // cálculo da média
 const media = (nota1 + nota2 + nota3)/3;
+
+document.getElementById('resultado').innerHTML = 'A média do aluno é: ' + media +'<br>';
 
 console.log('A nota 1 é:' , nota1)
 console.log('A nota 2 é:' , nota2)
@@ -21,4 +26,18 @@ console.log('A nota 3 é:' , nota3)
 
 console.log('A média do aluno é:' , media);
 
+if (media >= 7) {
+    document.getElementById('resultado').innerHTML += 'Situação: APROVADO';
+    // console.log('APROVADO'); QUANDO ERA PARA VER NO CONSOLE
+} else if (media < 7 && media >= 5) {
+    // console.log('EXAME'); QUANDO ERA PARA VER NO CONSOLE
+    document.getElementById('resultado').innerHTML += 'Situação: EXAME';
+} else {
+    document.getElementById('resultado').innerHTML += 'Situação: REPROVADO';
+    // console.log('REPROVADO'); QUANDO ERA PARA VER NO CONSOLE
+}
+
+
 console.log('Fim do Programa')
+
+}
