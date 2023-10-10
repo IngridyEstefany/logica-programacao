e681e30b1c0106b94eaa6b92e4342fd120a6827c
Ingridy Estefany <142455701+IngridyEstefany@users.noreply.github.com>
2023-10-10 19:48:08 +0000

    feat código da urna eletrônica

--- exemplo.js
+++ exemplo.js
@@ -3,3 +3,68 @@ console.log ("Olá, Console!");
 document.write("Olá, documento!");
 
 window.alert("Olá, Janela!")
+
+
+
+//URNA ELETRONICA
+// Estrutura: switch-case (escolha caso)
+
+const opcao =1;
+
+switch(opcao) {
+
+    case 1:
+        console.log('Opção é  1');
+        break;
+    case 2:
+        console.log('Opção é  2');
+        break;
+    default:
+        console.log('Nenhuma das opções');
+        break; //opcional
+}
+
+//equivalente à estrutura switch-case
+if (opcao === 1) {
+    console.log('Opção é 1');
+} else if (opcao === 2) {
+    console.log('Opção é 2');
+} else {
+    console.log('Nenhuma das opções');
+}
+
+//Estrutura de repetição: do-while (faça-enquanto)
+
+const opcao = 0;
+
+do {
+    console.log('Instrução 1');
+    console.log('Instrução 2');
+    console.log('Instrução 3');
+    console.log('Instrução 4');
+    console.log('Instrução 5');
+    console.log('Instrução 6');
+} while(opcao  !== 0);
+
+let contador = 0;
+
+do {
+    console.log ('Instrução com o contador em:', contador);
+    contador += 1;
+} while (contador <=10);
+
+
+
+//Estrutura de repetição: while (enquanto)
+
+const opcao = 0;
+
+while(opcao  !== 0); {
+    console.log('Instrução 1');
+    console.log('Instrução 2');
+    console.log('Instrução 3');
+    console.log('Instrução 4');
+    console.log('Instrução 5');
+    console.log('Instrução 6');
+}
+    
--- urnaEletronica.js
+++ urnaEletronica.js
@@ -1,61 +1,19 @@
-// Estrutura: switch-case (escolha caso)
-
-const opcao =1;
-
-switch(opcao) {
-
-    case 1:
-        console.log('Opção é  1');
-        break;
-    case 2:
-        console.log('Opção é  2');
-        break;
-    default:
-        console.log('Nenhuma das opções');
-        break; //opcional
-}
-
-//equivalente à estrutura switch-case
-if (opcao === 1) {
-    console.log('Opção é 1');
-} else if (opcao === 2) {
-    console.log('Opção é 2');
-} else {
-    console.log('Nenhuma das opções');
-}
-
-//Estrutura de repetição: do-while (faça-enquanto)
-
-const opcao = 0;
-
-do {
-    console.log('Instrução 1');
-    console.log('Instrução 2');
-    console.log('Instrução 3');
-    console.log('Instrução 4');
-    console.log('Instrução 5');
-    console.log('Instrução 6');
-} while(opcao  !== 0);
-
-let contador = 0;
-
-do {
-    console.log ('Instrução com o contador em:', contador);
-    contador += 1;
-} while (contador <=10);
-
-
-
-//Estrutura de repetição: while (enquanto)
-
-const opcao = 0;
-
-while(opcao  !== 0); {
-    console.log('Instrução 1');
-    console.log('Instrução 2');
-    console.log('Instrução 3');
-    console.log('Instrução 4');
-    console.log('Instrução 5');
-    console.log('Instrução 6');
-}
+function urnaEletronica() {
     
+    //Aqui vai todo o código do programa
+
+    console.log('Iniciando o programa');
+
+    let totalVotosCandidato1 = 0;
+    let totalVotosCandidato2 = 0;
+    let totalVotosCandidato3 = 0;
+    let totalVotosBranco = 0;
+    let totalVotosNulo = 0;
+    
+    do {
+
+        //instruções
+
+    } while (condicao)
+
+}
